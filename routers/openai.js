97f784a5b2de7909ce8d8fe5834e5b5b7476dadb
HyeS00 <hyesookim1118@gmail.com>
2024-06-11 11:27:46 +0900

    ✨ :: gpt api 로직 구현

--- routers/openai.js
+++ routers/openai.js
@@ -1,29 +1,42 @@
 const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
-
-// Load the .env file if it exists
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
 require("dotenv").config();
+const app = express();
+app.use(bodyParser.json());
 
-// You will need to set these environment variables or edit the following values
 const endpoint = process.env.AZURE_OPENAI_ENDPOINT;
 const azureApiKey = process.env.AZURE_OPENAI_KEY;
+const deploymentId = "gpt-4o";
+const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
 
-async function main() {
+router.post('/activity-routine', async (req, res) => {
   console.log("== Chat Completions Sample ==");
 
-  const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
-  const deploymentId = "gpt-4o";
-  const result = await client.getChatCompletions(deploymentId, [
-    { role: "system", content: "You are an AI assistant that helps people find information." },
-    { role: "user", content: "아이유는 누구야?" }
-  ]);
+  const { basicInfo, eatingHabit, activityHabit, activityGoal } = req.body;
+  const prompt = `Create a 1-week exercise routine for a ${basicInfo.age} year old ${basicInfo.gender} who wants to reach ${activityGoal.goalWeight}kg with a goal of ${activityGoal.activityGoal}. The user currently exercises ${activityHabit.exerciseFrequency} and eats a ${eatingHabit.dietType} diet. The user prefers ${activityGoal.preferenceActivity} at ${activityGoal.activityPlace} and plans to exercise ${activityGoal.activityFrequency}.`;
 
-  for (const choice of result.choices) {
-    console.log(choice.message);
+  try {
+    const result = await client.getChatCompletions(deploymentId, [
+      { role: "system", content: "You are an AI assistant that helps people find information." },
+      { role: "user", content: prompt }
+    ]);
+
+    const responses = result.choices.map((choice) => choice.message.content);
+
+    res.json({
+      success: true,
+      responses: responses, 
+      message: "성공"
+    })
+  } catch(err) {
+    res.json({
+      success: false,
+      response: [],
+      message: err
+    })
   }
-}
+})
 
-main().catch((err) => {
-  console.error("The sample encountered an error:", err);
-});
-
-module.exports = { main };
+module.exports = router;
