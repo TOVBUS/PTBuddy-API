fe1df2e1f1de86e10c2090fc3551f7b651d28c0a
doyeonjeong <debby_@kakao.com>
2024-06-13 14:18:57 +0900

    ✨ :: can pnpm start

--- ptbuddy-api/src/app.module.ts
+++ ptbuddy-api/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ActivityModule } from './activity/activity.module';
+import { RoutineModule } from './routine/routine.module';
+import { OpenaiModule } from './openai/openai.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [ActivityModule, RoutineModule, OpenaiModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
