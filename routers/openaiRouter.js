733d50a5e68e71ba54c3f2cecdaddef234ca8ef3
jhpa0308 <jhpa0308@gmail.com>
2024-06-13 09:47:33 +0900

    ➕ :: openAI Router 에meal-routine 추가

--- routers/openaiRouter.js
+++ routers/openaiRouter.js
@@ -77,4 +77,73 @@ router.post('/activity-routine', async (req, res) => {
   }
 })
 
+//식단 루틴 생성
+router.post('/meal-routine', async (req, res) => {
+  console.log("== Chat Completions Sample ==");
+
+  const { basicInfo, eatingHabit, activityHabit, activityGoal } = req.body;
+  const prompt = `사용자 정보:
+  - 이름: ${basicInfo.nick}
+  - 성별: ${basicInfo.gender}
+  - 생년월일: ${basicInfo.birth}
+  - 키: ${basicInfo.height}cm
+  - 체중: ${basicInfo.weight}kg
+  - 목표 몸무게: ${activityGoal.goalWeight}kg
+  - 목표 기간: 7일
+  - 하루 식사 횟수: ${eatingHabit.mealCount}
+  - 한 끼 섭취량: ${eatingHabit.mealAmout}
+  - 탄수화물 섭취 횟수: ${eatingHabit.carbohydratePerMealCount}
+  - 단백질 섭취 횟수: ${eatingHabit.proteinPerMealCount}
+  - 채소 섭취 횟수: ${eatingHabit.vegetablePerMealCount}
+  - 하루 물 섭취량: ${eatingHabit.waterIntakePerDay}
+  - 술 섭취량: ${eatingHabit.alcoholIntake}
+  - 평소 움직임: ${activityHabit.usualActivity}
+  - 하루 걸음 수: ${activityHabit.dailyStep}
+  - 무릎대고 푸쉬업 개수: ${activityHabit.pushUpCount}
+  - 규칙적 운동 기간: ${activityHabit.isRegularActivity}
+  - 규칙적 운동 유형: ${activityHabit.regularActivity}
+  - 일주일 운동 일수: ${activityHabit.regularActivityCount}
+  - 하루 운동 시간: ${activityHabit.activityTimePerDay}
+  - 운동량 변화: ${activityHabit.changeActivity}
+  - 주요 목표: ${activityGoal.activityGoal}
+  - 주 운동 장소: ${activityGoal.activityPlace}
+  - 선호 운동: ${activityGoal.preferenceActivity}
+  
+  위 사용자 정보를 바탕으로 7일간의 목표 기간 동안 한주 단위로 식단 루틴을 아래 예시대로 작성해주세요.
+  
+  예시 출력 형식:
+  1주치 식단 루틴:
+  - 월요일: ...
+  - 화요일: ...
+  - 수요일: ...
+  - 목요일: ...
+  - 금요일: ...
+  - 토요일: ...
+  - 일요일: ...
+  `
+
+  try {
+    const result = await client.getChatCompletions(deploymentId, [
+      { role: "system", content: "You are an AI assistant that helps people find information." },
+      { role: "user", content: prompt }
+    ]);
+
+    const responses = result.choices.map((choice) => choice.message.content);
+
+    res.json({
+      success: true,
+      responses: responses, 
+      message: "성공"
+    })
+  } catch(err) {
+    res.json({
+      success: false,
+      response: [],
+      message: err
+    })
+  }
+})
+
+
+
 module.exports = router;
