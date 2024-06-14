5ea33c494d52869d7a0d255e9eceac0a8f3a2d0b
doyeonjeong <debby_@kakao.com>
2024-06-14 09:51:41 +0900

    🐛 :: id(uuid) 값 포함 전달

--- ptbuddy-api/src/routine/routine.controller.ts
+++ ptbuddy-api/src/routine/routine.controller.ts
@@ -25,65 +25,68 @@ export class RoutineController {
   async generateAndSaveRoutine(@Body() body: UserInfo): Promise<any> {
     const prompt = createPrompt(body, 'routine');
     const responses = await this.openaiService.generateResponse(prompt);
+    const parsedResponse = this.parseResponse(
+      responses[0],
+      body.basicInfo.nick,
+    );
+    const savedRoutines = await this.routineService.createBulk(parsedResponse);
 
-    // OpenAI로부터 받은 응답을 그대로 반환
     return {
       success: true,
-      responses: responses[0],
+      routines: savedRoutines,
       message: '성공',
     };
   }
 
-  // private parseResponse(response: string, userId: string): Partial<Routine>[] {
-  //   const lines = response.split('||');
-  //   const parsedData: Partial<Routine>[] = [];
-  //   let week = 1;
-  //   let currentDay = '';
+  private parseResponse(response: string, userId: string): Partial<Routine>[] {
+    const lines = response.split('||');
+    const parsedData: Partial<Routine>[] = [];
+    let week = 1;
+    let currentDay = '';
 
-  //   lines.forEach((line) => {
-  //     const fields = line.split('|');
-  //     const dayField = fields.find((field) => field.startsWith('요일:'));
-  //     if (dayField) {
-  //       currentDay = dayField.split(':')[1];
-  //     }
-  //     const exerciseAreaField = fields.find((field) =>
-  //       field.startsWith('부위:'),
-  //     );
-  //     const exerciseNameENField = fields.find((field) =>
-  //       field.startsWith('이름(영어):'),
-  //     );
-  //     const exerciseNameKRField = fields.find((field) =>
-  //       field.startsWith('이름(한글):'),
-  //     );
-  //     const repsField = fields.find((field) => field.startsWith('횟수:'));
-  //     const setsField = fields.find((field) => field.startsWith('세트:'));
-  //     const weightField = fields.find((field) => field.startsWith('무게(kg):'));
-  //     const durationField = fields.find((field) =>
-  //       field.startsWith('시간(초):'),
-  //     );
+    lines.forEach((line) => {
+      const fields = line.split('|');
+      const dayField = fields.find((field) => field.startsWith('요일:'));
+      if (dayField) {
+        currentDay = dayField.split(':')[1];
+      }
+      const exerciseAreaField = fields.find((field) =>
+        field.startsWith('부위:'),
+      );
+      const exerciseNameENField = fields.find((field) =>
+        field.startsWith('이름(영어):'),
+      );
+      const exerciseNameKRField = fields.find((field) =>
+        field.startsWith('이름(한글):'),
+      );
+      const repsField = fields.find((field) => field.startsWith('횟수:'));
+      const setsField = fields.find((field) => field.startsWith('세트:'));
+      const weightField = fields.find((field) => field.startsWith('무게(kg):'));
+      const durationField = fields.find((field) =>
+        field.startsWith('시간(초):'),
+      );
 
-  //     if (exerciseAreaField && exerciseNameENField && exerciseNameKRField) {
-  //       const dayRoutine: Partial<Routine> = {
-  //         id,
-  //         userId,
-  //         week,
-  //         day: currentDay || 'Unknown',
-  //         exerciseArea: exerciseAreaField.split(':')[1] || 'Unknown',
-  //         exerciseNameEN: exerciseNameENField.split(':')[1] || 'Unknown',
-  //         exerciseNameKR: exerciseNameKRField.split(':')[1] || 'Unknown',
-  //         reps: repsField ? repsField.split(':')[1] : 'Unknown',
-  //         sets: setsField ? setsField.split(':')[1] : 'Unknown',
-  //         weight: weightField ? weightField.split(':')[1] : 'Unknown',
-  //         duration: durationField ? durationField.split(':')[1] : 'Unknown',
-  //       };
-  //       parsedData.push(dayRoutine);
-  //     }
+      if (exerciseAreaField && exerciseNameENField && exerciseNameKRField) {
+        const dayRoutine: Partial<Routine> = {
+          userId,
+          week,
+          day: currentDay || 'Unknown',
+          exerciseArea: exerciseAreaField.split(':')[1] || 'Unknown',
+          exerciseNameEN: exerciseNameENField.split(':')[1] || 'Unknown',
+          exerciseNameKR: exerciseNameKRField.split(':')[1] || 'Unknown',
+          reps: repsField ? repsField.split(':')[1] : 'Unknown',
+          sets: setsField ? setsField.split(':')[1] : 'Unknown',
+          weight: weightField ? weightField.split(':')[1] : 'Unknown',
+          duration: durationField ? durationField.split(':')[1] : 'Unknown',
+        };
+        parsedData.push(dayRoutine);
+      }
 
-  //     if (currentDay === '일요일') {
-  //       week++;
-  //     }
-  //   });
+      if (currentDay === '일요일') {
+        week++;
+      }
+    });
 
-  //   return parsedData;
-  // }
+    return parsedData;
+  }
 }
